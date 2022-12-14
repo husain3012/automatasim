c0ed88021884597378fa16cb0fc93b1001b10c8b
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2022-12-15 03:42:54 +0530

    responsiveness

--- components/DFA/DFA.tsx
+++ components/DFA/DFA.tsx
@@ -8,12 +8,12 @@ const DFA = () => {
   const [activeEdge, setActiveEdge] = useState(null);
 
   return (
-    <div className="flex flex-col sm:flex-row  w-screen h-[80vh]">
-      <div className="h-[30vh] sm:h-full   w-full sm:w-3/5  p-2">
+    <div className="flex flex-col sm:flex-row  ">
+      <div className="h-[30vh] sm:h-[90vh]   w-full sm:w-3/5  px-2">
         <Canvas dfa={dfa} activeEdge={activeEdge} />
       </div>
-      <div className="divider divider-horizontal"></div>
-      <div className="h-full p-2">
+      <div className="divider sm:divider-horizontal"></div>
+      <div className="mx-auto flex-col content-center sm:h-[90vh] m-2 p-2  sm:overflow-y-scroll scrollbar-thin scrollbar-thumb-base-300 scrollbar-track-base-200  ">
         <ControlPanel dfa={dfa} setActiveEdge={setActiveEdge} activeEdge={activeEdge} />
       </div>
     </div>
